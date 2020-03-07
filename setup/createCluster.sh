64944dd016816aa9c6a88448929016071771304b
nic <nwanavit@hatari.cc>
2020-03-07 08:42:40 +0700

    add new cluster

---
+++ setup/createCluster.sh
@@ -0,0 +1,5 @@
+eksctl create cluster \
+--name udagram \
+--version 1.14 \
+--region us-east-1 \
+--fargate
---
+++ updateAwsRole.sh
@@ -0,0 +1,1 @@
+aws eks update-kubeconfig --name udagram --region us-east-1 --role-arn arn:aws:iam::277726656832:role/ekscluster
